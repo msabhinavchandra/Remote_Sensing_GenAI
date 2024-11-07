5bccb7d72fcc679610f1de9ab2e13947ca9783d3
msabhinavchandra <msabhinavchandra@gmail.com>
2024-11-07 16:26:19 +0530

    a

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import '../Pages/SplashScreen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import dotenv
 // Import the DefaultFirebaseOptions class
 import 'firebase_options.dart';
+import 'package:network_info_plus/network_info_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +13,23 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
  // await dotenv.load(fileName: "Backend/.env"); // Load the .env file
   runApp(MyApp());
+}
+
+Future<String> _getIpAddress() async {
+  final info = NetworkInfo();
+  String? wifiIPv4;
+
+  if (await Permission.locationWhenInUse.request().isGranted) {
+    try {
+      wifiIPv4 = await info.getWifiIP();
+    } catch (e) {
+      wifiIPv4 = 'Failed to get IP';
+    }
+  } else {
+    wifiIPv4 = 'Permission denied';
+  }
+
+  return wifiIPv4 ?? 'Unknown IP';
 }
 
 //Backend\.env
